7a9618390f935a2255d5d19b9dba33734c069385
n.shilyaev <frederol123@gmail.com>
2026-05-10 15:45:25 +0300

    MinIO для хранения файлов; лимит изображений галереи (max_gallery_images default 6); фикс замыканий useMutation

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'max_gallery_images'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -23,6 +23,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'max_gallery_images' => 'integer',
         ];
     }
 
