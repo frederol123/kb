61d669d451dc234cd2e1d86c156dcc533fa10185
root <root@circular-apricot.ptr.network>
2026-07-28 22:11:46 +0000

    [fix]osn fix

--- app/Services/RobokassaService.php
+++ app/Services/RobokassaService.php
@@ -62,7 +62,7 @@ class RobokassaService
         $sum = number_format($amount, 2, '.', '');
 
         $data = [
-            'sno' => 'usn_income',  // Система налогообложения (обязательное поле для Robokassa)
+            'sno' => '',  // Система налогообложения (обязательное поле для Robokassa)
             'items' => [
                 [
                     'name'           => $name,
